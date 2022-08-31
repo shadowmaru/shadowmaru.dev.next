236d55de92d254bd7df5b6e38373eae2d09c259f
Ricardo Shiota Yasuda <shadowmaru@users.noreply.github.com>
2022-09-01 01:30:40 +0200

    Fixes Cypress tests

--- cypress/integration/post.spec.js
+++ cypress/integration/post.spec.js
@@ -1,7 +1,7 @@
 context('Post', () => {
   it('show the post title', () => {
     cy.visit('/') // for now testing with real posts, TODO: posts fixtures
-    cy.get('a.writing__posts__link').click()
+    cy.get('a.writing__posts__link').last().click()
 
     cy.contains("Using Notion for 1:1s")
   })
