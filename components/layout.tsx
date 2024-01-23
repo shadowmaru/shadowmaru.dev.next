19684bf5b912a6646f18987e71e989f30bd8c240
Ricardo Shiota Yasuda <shadowmaru@users.noreply.github.com>
2024-01-23 07:47:58 -0300

    Upgrade to NextJS 14

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import Image from "next/image";
 import styles from "../styles/layout.module.css";
 import utilStyles from "../styles/utils.module.css";
@@ -33,62 +34,58 @@ export default function Layout({
             content={`https://og-image.vercel.app/${encodeURI(
               siteTitle
             )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-            />
-              <meta name="og:title" content={siteTitle} />
-              <meta name="twitter:card" content="summary_large_image" />
-              <script>
-              {`
-                var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101172554);
-                `}
-                </script>
-                <script async src="//static.getclicky.com/js" />
-                  </Head>
-                  <header className={styles.header}>
-                    {home ? (
-                      <>
-                        <Image
-                          priority
-                          src="/images/profile.jpeg"
-                          className={utilStyles.borderCircle}
-                          height={144}
-                          width={144}
-                          alt={name}
-                        />
-                        <h1 className={utilStyles.headingXl}>{name}</h1>
-                      </>
-                    ) : (resume ? (
-                      <></>
-                    ): (
-                    <>
-                    <Link href="/">
-
-                      <Image
-                      priority
-                      src="/images/profile.jpeg"
-                      className={utilStyles.borderCircle}
-                      height={108}
-                      width={108}
-                      alt={name}
-                      />
-
-                    </Link>
-                    <h2 className={utilStyles.headingLg}>
-                    <Link href="/" className={utilStyles.colorInherit}>
-                    {name}
-                    </Link>
-                    </h2>
-                    </>
-                    ))}
-                    </header>
-                    <main>{children}</main>
-                    {!home && (
-                      <div className={styles.backToHome}>
-                      <Link href="/">
-                      ← Back to home
-                      </Link>
-                      </div>
-                    )}
-                    </div>
-                    </div>
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <Script>
+            {`
+            var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101172554);
+            `}
+          </Script>
+          <Script async src="//static.getclicky.com/js" />
+        </Head>
+        <header className={styles.header}>
+          {home ? (
+            <>
+              <Image
+                priority
+                src="/images/profile.jpeg"
+                className={utilStyles.borderCircle}
+                height={144}
+                width={144}
+                alt={name}
+              />
+              <h1 className={utilStyles.headingXl}>{name}</h1>
+            </>
+          ) : resume ? (
+            <></>
+          ) : (
+            <>
+              <Link href="/">
+                <Image
+                  priority
+                  src="/images/profile.jpeg"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </Link>
+              <h2 className={utilStyles.headingLg}>
+                <Link href="/" className={utilStyles.colorInherit}>
+                  {name}
+                </Link>
+              </h2>
+            </>
+          )}
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">← Back to home</Link>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
