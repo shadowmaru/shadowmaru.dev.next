eec0ac65c5b11378d032c0a4aa17e7ab8f5b35dc
Ricardo Shiota Yasuda <shadowmaru@users.noreply.github.com>
2025-05-20 19:07:08 +0200

    Fixes cypress test

--- cypress/e2e/post.cy.js
+++ cypress/e2e/post.cy.js
@@ -1,16 +1,19 @@
-context('Post', () => {
-  it('show the post title', () => {
-    cy.visit('/') // for now testing with real posts, TODO: posts fixtures
-    cy.get('a.writing__posts__link').last().click()
+context("Post", () => {
+  it("show the post title", () => {
+    cy.visit("/"); // for now testing with real posts, TODO: posts fixtures
+    cy.get("a.writing__posts__link").last().click();
 
-    cy.contains("Using Notion for 1:1s")
-  })
+    cy.contains("Using Notion for 1:1s");
+  });
 
-  it('has a title metadata with the post title', () => {
-    cy.visit('/') // for now testing with real posts, TODO: posts fixtures
-    cy.title().should("eq", "Ricardo Yasuda, software developer")
-    cy.get('a.writing__posts__link').last().click()
+  it("has a title metadata with the post title and the site title", () => {
+    cy.visit("/"); // for now testing with real posts, TODO: posts fixtures
+    cy.title().should("eq", "Ricardo Yasuda, software developer");
+    cy.get("a.writing__posts__link").last().click();
 
-    cy.title().should("eq", "Using Notion for 1:1s")
-  })
-})
+    cy.title().should(
+      "eq",
+      "Using Notion for 1:1s | Ricardo Yasuda, software developer",
+    );
+  });
+});
