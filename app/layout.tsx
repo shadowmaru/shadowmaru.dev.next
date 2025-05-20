4482433c7b7222d95fdd4466e442db5263d05dc4
Ricardo Shiota Yasuda <shadowmaru@users.noreply.github.com>
2025-05-20 18:56:37 +0200

    Combine title post and site title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import '../styles/global.css';
+import "../styles/global.css";
 
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 
-const siteTitle = "Ricardo Yasuda, software developer"
+const siteTitle = "Ricardo Yasuda, software developer";
 const siteUrl =
   process.env.NODE_ENV === "production"
     ? "https://yasuda.dev.br"
@@ -10,9 +10,9 @@ const siteUrl =
 
 export const metadata: Metadata = {
   title: siteTitle,
-  description: 'Ricardo Yasuda, software developer and engineering manager',
+  description: "Ricardo Yasuda, software developer and engineering manager",
   icons: {
-    icon: '/favicon.ico',
+    icon: "/favicon.ico",
   },
   openGraph: {
     title: siteTitle,
@@ -21,35 +21,35 @@ export const metadata: Metadata = {
     images: [
       {
         url: `https://og-image.vercel.app/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`
-      }
+          siteTitle,
+        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`,
+      },
     ],
-    type: 'website',
+    type: "website",
   },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
   },
   other: {
-    'fediverse:creator': `@shadowmaru@bolha.one`
+    "fediverse:creator": `@shadowmaru@bolha.one`,
   },
   alternates: {
     types: {
-      'application/rss+xml': `${siteUrl}/feed.xml`,
+      "application/rss+xml": `${siteUrl}/feed.xml`,
     },
-  }
-}
+  },
+};
 
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,17 +1,17 @@
-import { Metadata, ResolvingMetadata } from 'next';
-import { metadata } from '../../layout';
-import Date from '../../../components/date';
-import { getPostData } from '../../../lib/posts';
-import utilStyles from '../../../styles/utils.module.css';
+import { Metadata, ResolvingMetadata } from "next";
+import { metadata } from "../../layout";
+import Date from "../../../components/date";
+import { getPostData } from "../../../lib/posts";
+import utilStyles from "../../../styles/utils.module.css";
 
 type Props = {
-  params: Promise<{ id: string }>
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
 
 export default async function Post(props) {
   const params = await props.params;
-  const postData = await getPost(params)
+  const postData = await getPost(params);
   return (
     <article>
       <h1 className={utilStyles.headingXl}>{postData.title}</h1>
@@ -25,28 +25,30 @@ export default async function Post(props) {
 
 export async function generateMetadata(
   { params, searchParams }: Props,
-  parent: ResolvingMetadata
+  parent: ResolvingMetadata,
 ): Promise<Metadata> {
-  const id = (await params).id
+  const id = (await params).id;
 
-  const post = await getPostData(id as string)
+  const post = await getPostData(id as string);
 
-  const thumbnail = post.thumbnail ? `${metadata.openGraph?.url}${post.thumbnail}` : null
+  const thumbnail = post.thumbnail
+    ? `${metadata.openGraph?.url}${post.thumbnail}`
+    : null;
 
   return {
-    title: post.title,
+    title: `${post.title} | ${metadata.title}`,
     description: post.description,
     ...(thumbnail && {
       openGraph: {
-       images: [thumbnail],
-      }
+        images: [thumbnail],
+      },
     }),
     ...(post.author && {
       other: {
-        'fediverse:creator': post.author,
+        "fediverse:creator": post.author,
       },
     }),
-  }
+  };
 }
 
 async function getPost(params) {
